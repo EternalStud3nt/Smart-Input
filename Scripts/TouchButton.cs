a9832174ea7748bb56f4e1a5b67f79a1231657e9
Eternal Student <eternalstud3nt@gmail.com>
2024-07-18 01:06:01 +0300

    Changed name to touch input

--- Scripts/TouchButton.cs
+++ Scripts/TouchButton.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 
 namespace SmartInput
 {
-    public class SmartButton  : MonoBehaviour
+    public class TouchButton  : MonoBehaviour
     {
         public UnityEvent OnButtonHold = new UnityEvent();
         public UnityEvent OnButtonPress = new UnityEvent();
